a3fbb82d6def40b39495415c0538092247269e6f
Pedro Pegoraro Germano <pedrogermano@indg.com.br>
2020-03-22 22:41:00 -0300

    novo teste no arquivos de teste

--- decorator_test.py
+++ decorator_test.py
@@ -3,14 +3,24 @@ referencia:
 https://stackoverflow.com/questions/14703310/how-can-i-get-a-python-decorator-to-run-after-the-decorated-function-has-complet
 '''
 
-def audit_action(action):
+
+# def audit_action(function_to_decorate):
+#     def wrapper(*args, **kw):
+#         # Calling your function
+#         output = function_to_decorate(*args, **kw)
+#         # Below this line you can do post processing
+#         print("In Post Processing....")
+#         args[0].inicial_um = 2
+#         return output
+#     return wrapper
+
+def audit_action():
     def decorator_func(func):
         def wrapper_func(*args, **kwargs):
-            print(args)
             # Invoke the wrapped function first
             retval = func(*args, **kwargs)
             # Now do something here with retval and/or action
-            print('In wrapper_func, handling action {!r} after wrapped function returned {!r}'.format(action, retval))
+            print('In wrapper_func, handling action  after wrapped function returned {!r}'.format(retval))
             args[0].inicial_um = 2
             return retval
         return wrapper_func
@@ -22,13 +32,13 @@ class Teste():
     def __init__(self):
         self.inicial_um = 1
 
-    @audit_action(action='did something')
-    def do_something(*args, **kwargs):
+    @audit_action()
+    def do_something(self, *args, **kwargs):
         if args[0] == 'foo':
             return 'bar'
         else:
             return 'baz'
 
 t = Teste()
-t.do_something('')
+t.do_something('asd')
 print(t.inicial_um)
